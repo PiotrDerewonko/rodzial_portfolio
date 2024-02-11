d73eb0bdd4415fa3ed0b39d8880a020a9d901397
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-02-11 13:52:18 +0100

    dzialajace zmienne pozwalajace na podswietlenie sekcji w ktorej jestesmy

--- infos/views.py
+++ infos/views.py
@@ -4,15 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     t = loader.get_template("infos/main.html")
-    c = {"test": "dodawanie", 'title': 'Homepage'}
+    c = {'title': 'Homepage', 'active_tab': 'home'}
     return HttpResponse(t.render(c))
 
 def about_me(request):
     t = loader.get_template("infos/about_me.html")
-    c = {'title': 'O mnie'}
+    c = {'title': 'O mnie', 'active_tab': 'me'}
     return HttpResponse(t.render(c))
 
 def contact(request):
     t = loader.get_template("infos/contact.html")
-    c = {'title': 'Kontakt'}
+    c = {'title': 'Kontakt', 'active_tab': 'contact'}
     return HttpResponse(t.render(c))
